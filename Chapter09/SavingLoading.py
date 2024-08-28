0d7ff7b151f7cd272863384d0c6446c5ba64be54
Deepak Gowda <reachdeepak@gmail.com>
2024-08-29 05:28:59 +0530

    Update SavingLoading.py

--- Chapter09/SavingLoading.py
+++ Chapter09/SavingLoading.py
@@ -1,1 +1,9 @@
+#saving the model
+from pyspark.ml.classification import LogisticRegressionModel  
+model_path = "/path/to/save/model"  
+model.save(model_path) 
 
+#loading the model
+from pyspark.ml.classification import LogisticRegressionModel  
+model_path = "/path/to/save/model"  
+loaded_model = LogisticRegressionModel.load(model_path) 
